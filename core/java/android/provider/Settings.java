4ef1566cdaa44246f10f3b3188053f6a0d34c7d2
Alex Cruz <mazdarider23@gmail.com>
2015-05-25 14:28:26 +0200

    Show 4G instead of LTE switch [1/2]
    Change-Id: I61b0d04cc3b2611ed9818ba736bfe09150987f46
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3553,6 +3553,12 @@ public final class Settings {
         public static final String STATUSBAR_COMPONENT = "statusbar_component";
 
         /**
+         * Show four g instead of lte
+         * @hide
+         */
+        public static final String SHOW_FOURG = "show_fourg";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
